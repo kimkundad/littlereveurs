81dbbada68da2c0388c88949d1c5a4f795f21abb
kimkundad <kim.kundad@gmail.com>
2018-01-31 19:43:27 +0700

    first commit

--- resources/views/layouts-front/template.blade.php
+++ resources/views/layouts-front/template.blade.php
@@ -28,7 +28,7 @@
 
 
 
-    @include('layouts-front.inc-header')
+  <!--   @include('layouts-front.inc-header') -->
 
 
 
@@ -46,7 +46,7 @@
 
 
 
-    @include('layouts-front.inc-footer')
+  <!--  @include('layouts-front.inc-footer') -->
 
     <!-- JavaScripts -->
     @include('layouts-front.inc-script')
