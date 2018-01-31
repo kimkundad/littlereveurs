b5e102a50e2bf39334b608c70f60aa7e0546f1ca
kimkundad <kim.kundad@gmail.com>
2018-01-31 20:03:57 +0700

    first commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,442 @@
 @extends('layouts-front.template')
 
+
+@section('title')
+The Original Real Estate CASHBACK Network | Siri Space
+@stop
+
+@section('description')
+SIRISPACE.com is a real estate company located in Bangkok, Thailand that helps connect property hunters with the most current database of commercial and residential properties for sale and rent, with property news and all the resources a buyer
+@stop
+
+@section('ogtags')
+    @include('layouts.og_tags', ['title' => 'The Original Real Estate CASHBACK Network | Siri Space', 'description' => 'SIRISPACE.com is a real estate company located in Bangkok,
+    Thailand that helps connect property hunters with the most current database of commercial and residential properties for sale and rent, with property news and all the resources a buyer',
+    'image' => url('assets/image/sirispace_facebook.png')])
+@stop
+
+
+<style>
+.list_menu_y {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    background: url(assets/font-end/image/arrow_black_bottom.png) no-repeat right;
+    background-size: 12px 6px;
+    width: 150px;
+    border: none !important;
+    font-family: "Tahoma", "Geneva", "sans-serif";
+    font-size: 14px;
+    color: #555555;
+    font-weight: normal;
+    line-height: 1.2em;
+    outline: none !important;
+    padding: 5px;
+}
+.list_menu_y select:hover{
+  border: none;
+}
+.bs-example {
+    margin-right: 0;
+    margin-left: 0;
+
+    border-color: #ddd;
+    border-width: 1px;
+    border-radius: 4px 4px 0 0;
+    -webkit-box-shadow: none;
+    box-shadow: none;
+}
+@media (max-width: 767px){
+
+  .promo_full_wp>div {
+      display: table-cell;
+      padding: 0% 0%;
+      vertical-align: middle;
+      text-align: center;
+      color: #fff;
+      font-size: 16px;
+  }
+  .form-group {
+    margin-bottom: 8px;
+}
+.logo-style{
+  margin-top: 10px;
+    margin-bottom: 10px;
+}
+
+}
+
+
+/*  */
+</style>
+
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="content-section-b">
+        <div class="container" >
+
+          <div class="row hidden-sm hidden-xs">
+        <div class="col-md-12 " >
+          <a href="#" style="width:100%;height:180px;display:block;">
+            <img src="{{url('assets/font-end/banner/'.$setting->image)}}" class="img-responsive" style="border: 1px solid #e0e0e0; margin: 0 auto;">
+          </a>
+        </div>
+        </div>
+
+          <div class="row">
+            <br>
+        <div class="col-md-12 " >
+          <h3>SIRI RECOMMEND</h3>
+          <p>Find the hottest property in Bangkok</p>
+
+
+
+          <div class="body-project">
+
+                    <div class="row">
+
+
+                      <div class="col-sm-4 col-md-3">
+
+                            <div class="thumbnail a_sd_move">
+                              <div style="max-height: 184px; min-height: 184px; overflow: hidden; position: relative;">
+                              <a href="https://www.sirispace.com/asset-1357">
+                              <img src="https://www.sirispace.com/assets/cusimage/1516979121.jpg">
+                              <div class="g_l_box">
+                                    <div class="t_white">
+                                    <p>
+                                  <b class="t18 t_white">฿80000</b>
+                                    </p>
+                                      </div>
+                                  </div>
+                              </a></div>
+                              <div class="caption" style="padding: 3px;">
+                                <div class="descript bold">
+                                    <a href="https://www.sirispace.com/asset-1357">Millennium Residence</a>
+                                </div>
+                                <div class="descript" style="padding-bottom: 5px;color: #777; font-size: 12px;border-bottom: 1px dashed #dff0d8;">
+                                  2+1 bedrooms 3 bathrooms on 2...                            </div>
+
+                                <div class="descript" style="height: 20px;">
+                                  <span style="color: #e03753; font-size: 12px;"><i class="fa fa-map-marker"></i> Khlong Toei</span>
+                                  <div class="descript-t">
+                                  <div class="postMetaInline-authorLockup">
+
+                                                                    <div class="rating">
+                                        <i class="fa fa-star voted"></i>
+                                        <i class="fa fa-star voted"></i>
+                                        <i class="fa fa-star voted"></i>
+                                        <i class="fa fa-star voted"></i>
+                                        <i class="fa fa-star voted"></i>
+                                        <span style="color: #777; font-size: 12px;">5.0</span>
+                                    </div>
+
+                                  </div>
+                                  </div>
+                                </div>
+
+                              </div>
+                            </div>
+                          </div>
+
+
+
+
+
+
+
+
+                    </div>
+
+
+
+          </div>
+        <!--    <div class="panel panel-default">
+                <div class="panel-heading">Welcome</div>
 
                 <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                    Your Application's Landing Page.
                 </div>
-            </div>
+            </div> -->
+
+
+
         </div>
+
+
+<style>
+.view-more-wrapper {
+    margin-bottom: 0px;
+}
+.view-more-wrapper {
+    text-align: center;
+    margin-top: 22px;
+}
+.view-more {
+    text-decoration: none !important;
+    text-transform: uppercase;
+    font-size: 12px;
+    line-height: 30px !important;
+    color: #666 !important;
+    border-color: #666 !important;
+    display: inline-block;
+}
+.view-more .plus-sign {
+    display: inline-block;
+    width: 60px;
+    height: 60px;
+    margin: 0 auto;
+    padding: 5px;
+    border: 1px solid #666;
+    font-size: 46px;
+    font-weight: 100;
+    line-height: 46px;
+    text-align: center;
+    border-bottom-left-radius: 50%;
+    border-top-left-radius: 50%;
+    border-bottom-right-radius: 50%;
+    border-top-right-radius: 50%;
+}
+.view-more .view-more-text {
+    display: inline-block;
+    width: 100%;
+    text-align: center;
+}
+</style>
+
+        <div class="col-xs-12">
+                <div class="view-more-wrapper"><a href="{{url('siri_recommend')}}" class="view-more"><span class="plus-sign">+</span><span class="view-more-text">View More Posts</span></a></div>
+              </div>
+
+
+
     </div>
+
+
+
+        </div>
 </div>
-@endsection
+
+
+
+<section class="promo_full">
+			<div class="promo_full_wp magnific">
+				<div>
+					<h3>#1 RENTAL PROFESIONAL</h3>
+					<h4 style="font-size: 28px;">
+						Welcome to your new home...
+					</h4>
+
+				</div>
+			</div>
+		</section>
+
+
+
+
+
+
+    <div class="content-section-b" style="padding: 20px 0 20px;">
+            <div class="container" >
+
+
+              <div class="row">
+                <br>
+            <div class="col-md-12 " >
+              <h3>RECENT UPDATES </h3>
+              <p>We're update a fresh listing everyday!</p>
+
+
+              <div class="body-project">
+
+                        <div class="row">
+
+
+
+
+                          <div class="col-sm-4 col-md-3">
+
+                        <div class="thumbnail a_sd_move">
+                          <div style="max-height: 184px; min-height: 184px; overflow: hidden; position: relative;">
+                          <a href="https://www.sirispace.com/asset-1357">
+                          <img src="https://www.sirispace.com/assets/cusimage/1516979121.jpg">
+                          <div class="g_l_box">
+                                <div class="t_white">
+                                <p>
+                              <b class="t18 t_white">฿80000</b>
+                                </p>
+                                  </div>
+                              </div>
+                          </a></div>
+                          <div class="caption" style="padding: 3px;">
+                            <div class="descript bold">
+                                <a href="https://www.sirispace.com/asset-1357">Millennium Residence</a>
+                            </div>
+                            <div class="descript" style="padding-bottom: 5px;color: #777; font-size: 12px;border-bottom: 1px dashed #dff0d8;">
+                              2+1 bedrooms 3 bathrooms on 2...                            </div>
+
+                            <div class="descript" style="height: 20px;">
+                              <span style="color: #e03753; font-size: 12px;"><i class="fa fa-map-marker"></i> Khlong Toei</span>
+                              <div class="descript-t">
+                              <div class="postMetaInline-authorLockup">
+
+                                                                <div class="rating">
+                                    <i class="fa fa-star voted"></i>
+                                    <i class="fa fa-star voted"></i>
+                                    <i class="fa fa-star voted"></i>
+                                    <i class="fa fa-star voted"></i>
+                                    <i class="fa fa-star voted"></i>
+                                    <span style="color: #777; font-size: 12px;">5.0</span>
+                                </div>
+
+                              </div>
+                              </div>
+                            </div>
+
+                          </div>
+                        </div>
+                      </div>
+
+
+
+
+
+
+
+
+
+
+
+
+                        </div>
+
+
+
+              </div>
+            <!--    <div class="panel panel-default">
+                    <div class="panel-heading">Welcome</div>
+
+                    <div class="panel-body">
+                        Your Application's Landing Page.
+                    </div>
+                </div> -->
+
+
+
+            </div>
+
+            <div class="col-xs-12">
+                    <div class="view-more-wrapper"><a href="{{url('recent_updates')}}" class="view-more"><span class="plus-sign">+</span><span class="view-more-text">View More Posts</span></a></div>
+                  </div>
+
+        </div>
+
+
+
+            </div>
+    </div>
+
+
+
+
+
+    <div class="content-section-b" style="padding: 20px 0 50px;">
+            <div class="container" >
+
+<hr style="border-top: 1px solid rgb(195, 202, 196);">
+              <div class="row">
+                <div class="col-md-12">
+<h3 class="text-center" style="font-weight:700">CERTIFICATE AND PARTNERSHIPS </h3>
+<br>
+</div>
+
+
+            <div class="col-md-2 col-sm-3 col-xs-6 logo-style">
+              <img alt="dealcha.com" src="assets/font-end/image/dealcha.png"></a>
+            </div>
+
+            <div class="col-md-2 col-sm-3 col-xs-6 logo-style" >
+
+            <div id="Certificate-banners"><a title="กรมพัฒนาธุรกิจการค้า sirispace" style="padding-left: 5px;">
+                <img alt="กรมพัฒนาธุรกิจการค้า sirispace" src="assets/font-end/image/bns_registered.png"></a>
+              </div>
+
+
+
+                <!--  <script src="https://www.trustmarkthai.com/callbackData/initialize.js?t=14f50-15-5-837d5ecc2533522de0fc24060ca019bf16612f49" id="dbd-init"></script><div id="Certificate-banners"></div> -->
+
+            </div>
+
+            <div class="col-md-2 col-sm-3 col-xs-6 logo-style" >
+                <img alt="กรมพัฒนาธุรกิจการค้า sirispace" src="assets/font-end/image/vbv.png"></a>
+            </div>
+
+            <div class="col-md-2 col-sm-3 col-xs-6 logo-style" >
+                <img alt="กรมพัฒนาธุรกิจการค้า sirispace" src="assets/font-end/image/jb.png"></a>
+            </div>
+
+            <div class="col-md-2 col-sm-3 col-xs-6 logo-style" >
+                <img alt="กรมพัฒนาธุรกิจการค้า sirispace" src="assets/font-end/image/whatsonsukhumvit.png"></a>
+            </div>
+            <div class="col-md-2 col-sm-3 col-xs-6 logo-style">
+              <img alt="dealcha.com" src="assets/font-end/image/tccc-logo.png"></a>
+            </div>
+
+        </div>
+
+
+
+            </div>
+    </div>
+@stop
+
+@section('scripts')
+
+<script>
+var button = document.getElementById('button_open');
+
+button.onclick = function() {
+    var div = document.getElementById('newpost');
+    if (div.style.display !== 'none') {
+        div.style.display = 'none';
+    }
+    else {
+        div.style.display = 'block';
+    }
+};
+</script>
+
+<script>
+var button = document.getElementById('button_open_res');
+
+button.onclick = function() {
+    var div = document.getElementById('newpost_res');
+    if (div.style.display !== 'none') {
+        div.style.display = 'none';
+    }
+    else {
+        div.style.display = 'block';
+    }
+};
+</script>
+
+
+@stop('scripts')
