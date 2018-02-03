53bba753f388c8a7fa384e3eed58f41d286c5cd0
kimkundad <kim.kundad@gmail.com>
2018-02-03 19:12:33 +0700

    first commit

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -297,7 +297,7 @@
             <hr>
 
             <p style="color:#888; font-size:12px; margin-top:10px; margin-bottom:0px;">
-              <b style="float:left;"> ขนาด :</b>
+              
               <a style="float:left; margin-right:8px;" class="btn btn-success btn-xs" role="button">S </a>
               <a style="float:left; margin-right:8px;" class="btn btn-success btn-xs" role="button">M </a>
               <a style="float:left; margin-right:8px;" class="btn btn-success btn-xs" role="button">L </a>
