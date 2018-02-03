d0b8b51b90eb9b4ca781d8cec29517e385e6a2b0
kimkundad <kim.kundad@gmail.com>
2018-02-03 18:32:03 +0700

    first commit

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -274,6 +274,26 @@
               {!! $home->product_detail !!}
             </p>
 
+            <?php
+            function DateThai($strDate)
+            {
+            $strYear = date("Y",strtotime($strDate))+543;
+            $strMonth= date("n",strtotime($strDate));
+            $strDay= date("j",strtotime($strDate));
+            $strHour= date("H",strtotime($strDate));
+            $strMinute= date("i",strtotime($strDate));
+            $strSeconds= date("s",strtotime($strDate));
+            $strMonthCut = Array("","ม.ค.","ก.พ.","มี.ค.","เม.ย.","พ.ค.","มิ.ย.","ก.ค.","ส.ค.","ก.ย.","ต.ค.","พ.ย.","ธ.ค.");
+            $strMonthThai=$strMonthCut[$strMonth];
+            return "$strDay $strMonthThai $strYear";
+            }
+            ?>
+
+            <p style="color:#888; font-size:12px; margin-top:10px; margin-bottom:0px;">
+              <i class="fa fa-clock"></i> วันที่อัพเดทสินค้า : 
+              <?php echo DateThai($home->created_at); ?>
+            </p>
+
 
 
 
