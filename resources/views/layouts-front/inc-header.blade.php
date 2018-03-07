0d3d1b478f854ad2f8011e1881c882b29af982f1
kimkundad <kim.kundad@gmail.com>
2018-03-07 19:56:16 +0700

    golf

--- resources/views/layouts-front/inc-header.blade.php
+++ resources/views/layouts-front/inc-header.blade.php
@@ -59,17 +59,65 @@
 }
 .logo-container {
     margin: 0 auto;
-    margin-bottom: 48px;
+    margin-bottom: 10px;
+}
+@media screen and (min-width:768px){
+    .navbar-brand-centered {
+        position: absolute;
+        left: 50%;
+        display: block;
+        width: 160px;
+        text-align: center;
+        background-color: #eee;
+    }
+    .navbar>.container .navbar-brand-centered,
+    .navbar>.container-fluid .navbar-brand-centered {
+        margin-left: -80px;
+    }
 }
 </style>
 <nav class=" hidden-sm hidden-xs">
     <div class="container">
 
-      <div class="header header-primary text-center">
+    <!--  <div class="header header-primary text-center">
                       <div class="logo-container">
                           <img src="{{url('assets/img/logo_website.jpg')}}" alt="">
                       </div>
-      </div>
+      </div> -->
+
+      <nav class="navbar navbar-default" role="navigation">
+    	  <div class="container">
+		    <!-- Brand and toggle get grouped for better mobile display -->
+		    <div class="navbar-header">
+		      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-brand-centered">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		      </button>
+		      <div class="navbar-brand navbar-brand-centered">
+            <div class="logo-container">
+                <img src="{{url('assets/img/logo_website.jpg')}}" alt="">
+            </div>
+        </div>
+		    </div>
+
+		    <!-- Collect the nav links, forms, and other content for toggling -->
+		    <div class="collapse navbar-collapse" id="navbar-brand-centered">
+		      <ul class="nav navbar-nav">
+		        <li><a href="#">Link</a></li>
+		        <li><a href="#">Link</a></li>
+		        <li><a href="#">Link</a></li>
+		      </ul>
+		      <ul class="nav navbar-nav navbar-right">
+		        <li><a href="#">Link</a></li>
+		        <li><a href="#">Link</a></li>
+		        <li><a href="#">Link</a></li>
+		      </ul>
+		    </div><!-- /.navbar-collapse -->
+		  </div><!-- /.container-fluid -->
+		</nav>
+
 
     </div>
 </nav>
